92aea637ab629c6fd4b84549f6c3bc3b3e4863c5
Mauro Ferreira <mauroferreiraa27@gmail.com>
2025-07-06 00:51:04 -0400

    Create alu.v

---
+++ ALU_Project/alu.v
@@ -0,0 +1,22 @@
+module alu(
+    input [31:0] a, b,
+    input [3:0] op_code,
+    output reg [31:0] result
+);
+
+always @(*) begin
+    case (op_code)
+    4'b0000: result = a + b;
+    4'b0001: result = a - b;
+    4'b0010: result = a & b;
+    4'b0011: result = a | b;
+    4'b0100: result = a ^ b;
+    4'b0101: result = ~a;
+    4'b0110: result = a << b;
+    4'b0111: result = a >> b;
+    default: result = 32'hDEADBEEF;
+    endcase
+
+end
+
+endmodule
